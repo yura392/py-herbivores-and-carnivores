4dcac9cb3945fc516edfad05cd98432862b06be8
yura <yura392@gmail.com>
2026-06-09 11:28:22 +0300

    solution 1

--- app/main.py
+++ app/main.py
@@ -1,23 +1,27 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         self.name = name
         self.health = 100
         self.hidden = False
         Animal.alive.append(self)
 
-    def __repr__(self):
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
-    def _check_death(self):
+    def _check_death(self) -> None:
         if self.health <= 0:
             self.health = 0
             if self in Animal.alive:
                 Animal.alive.remove(self)
 
     @classmethod
-    def __str__(cls):
+    def __str__(cls) -> str:
         return str(cls.alive)
 
 
